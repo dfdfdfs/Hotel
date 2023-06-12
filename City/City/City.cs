3d354fb386701822e5607d77dbd6e2b64157b275
Sasha <sasha.ilyasevich@mail.ru>
2023-06-12 22:06:45 +0300

    Add files via upload

---
+++ City/City/City.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.IO;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace City
+{
+    // Страны
+    public enum Country
+    {
+        Russia, // Россия
+        Kazakhstan, // Казахстан
+        Kyrgyzstan, // Кыргызстан
+        China // Китай
+    }
+
+    // Море
+    public enum Sea
+    {
+        Baltic,
+        Black,
+        Caspian,
+        Yellow
+    }
+
+    // Класс "Город"
+    public abstract class City
+    {
+        // Параметры города
+        protected string name; // Название 
+        protected DateTime date; // Дата основания
+        protected double area; // Площадь
+        protected int population; // Население
+        protected Country country; // Страна
+
+        // Сохранение в файл
+        public abstract void Save(string filename, string path);
+
+        // Получение информации
+        public abstract string Info();
+    }
+
+    // Закрытый город
+    public class SecretCity : City
+    {
+        protected string codename; // кодовое имя
+
+        // Конструктор закрытого города
+        public SecretCity(string n, DateTime d, double a, int p, Country c, string cn)
+        {
+            name = n;
+            date = d;
+            area = a;
+            population = p;
+            country = c;
+            codename = cn;
+        }
+
+        // Строковое представление
+        public override string ToString()
+        {
+            return $"Название: {name}. Дата основания: {date}. Площадь: {area}." +
+                $" Население: {population}. Страна: {country}." +
+                $" Кодовое название: {codename}.";
+        }
+
+        // Сохранение в файл
+        public override void Save(string filename, string path)
+        {
+            File.WriteAllText(path + "/" + filename, ToString());
+        }
+
+        // Получение информации
+        public override string Info()
+        {
+            TypeInfo t = typeof(SecretCity).GetTypeInfo();
+            IEnumerable<MethodInfo> mList = t.DeclaredMethods;
+
+            StringBuilder sb = new StringBuilder();
+
+            // Получение методов и хэш-кодов
+            sb.Append("Методы:" + Environment.NewLine);
+            foreach (MethodInfo m in mList)
+            {
+                sb.Append(m.DeclaringType.Name + ": " + m.Name + " " + m.GetHashCode() + Environment.NewLine);
+            }
+
+            return sb.ToString();
+        }
+    }
+
+    public class Harbor : City
+    {
+        protected Sea sea; // море
+
+        // Конструктор порта
+        public Harbor(string n, DateTime d, double a, int p, Country c, Sea s)
+        {
+            name = n;
+            date = d;
+            area = a;
+            population = p;
+            country = c;
+            sea = s;
+        }
+
+        // Строковое представление
+        public override string ToString()
+        {
+            return $"Название: {name}. Дата основания: {date}. Площадь: {area}." +
+                $" Население: {population}. Страна: {country}." +
+                $" Море: {sea}.";
+        }
+
+        // Сохранение в файл
+        public override void Save(string filename, string path)
+        {
+            File.WriteAllText(path + "/" + filename, ToString());
+        }
+
+        // Получение информации
+        public override string Info()
+        {
+            TypeInfo t = typeof(Harbor).GetTypeInfo();
+            IEnumerable<MethodInfo> mList = t.DeclaredMethods;
+
+            StringBuilder sb = new StringBuilder();
+
+            // Получение методов и хэш-кодов
+            sb.Append("Методы:" + Environment.NewLine);
+            foreach (MethodInfo m in mList)
+            {
+                sb.Append(m.DeclaringType.Name + ": " + m.Name + " " + m.GetHashCode() + Environment.NewLine);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ City/City/CityForm.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace City
+{
+    public partial class CityForm : Form
+    {
+        // Список гостиниц
+        private List<City> cities = new List<City>();
+
+        // Конструктор формы
+        public CityForm()
+        {
+            InitializeComponent();
+        }
+
+        // Действия при загрузке формы
+        private void CityForm_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = comboBox2.SelectedIndex
+                = comboBox3.SelectedIndex = 0;
+        }
+
+        // Действия при нажатии на кнопку
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Добавление закрытого города
+                if (comboBox1.SelectedIndex == 0)
+                    cities.Add(new SecretCity(textBox1.Text, dateTimePicker1.Value,
+                        double.Parse(textBox2.Text), int.Parse(textBox3.Text),
+                        (Country)comboBox2.SelectedIndex, textBox5.Text));
+                // Добавление порта
+                else
+                    cities.Add(new Harbor(textBox1.Text, dateTimePicker1.Value,
+                        double.Parse(textBox2.Text), int.Parse(textBox3.Text),
+                        (Country)comboBox2.SelectedIndex, (Sea)comboBox3.SelectedIndex));
+
+                // Сохранение данных в файл
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    cities[cities.Count - 1].Save(Path.GetFileName(saveFileDialog1.FileName),
+                        Path.GetDirectoryName(saveFileDialog1.FileName));
+                }
+                // Вывод данных в текстовое поле
+                textBox4.Text += cities[cities.Count - 1].Info() + Environment.NewLine;
+                textBox4.Text += cities[cities.Count - 1].ToString() + Environment.NewLine;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        // Действия при изменении типа города
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Включение режима Хостел или С завтраком
+            label7.Enabled = textBox5.Enabled = comboBox1.SelectedIndex == 0;
+            label8.Enabled = comboBox3.Enabled = comboBox1.SelectedIndex != 0;
+        }
+    }
+}
